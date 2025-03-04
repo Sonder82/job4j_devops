3f1712e2e3fd90709dffe20d314c33f24a0f09a3
Aleksey Novoselov <Himrs82@gmail.com>
2025-03-04 15:51:31 +0500

    Liquibase:rollback [#505366]

---
+++ src/main/resources/db/changelog/changeset/003_ddl_alter_add_user_create_date.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset anovoselov:add_user_create_date
+
+ALTER TABLE users
+ADD COLUMN create_date TIMESTAMP WITHOUT TIME ZONE DEFAULT now();
+
+--rollback ALTER TABLE users DROP COLUMN create_date;
