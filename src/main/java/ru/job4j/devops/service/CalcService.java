f8ac1846c8f09a370132ac9cb2baa2367925e465
Aleksey Novoselov <Himrs82@gmail.com>
2025-03-24 13:06:33 +0500

    Spring JPA и Testcontainers [#505371]

--- src/main/java/ru/job4j/devops/service/CalcService.java
+++ src/main/java/ru/job4j/devops/service/CalcService.java
@@ -1,13 +1,19 @@
 package ru.job4j.devops.service;
 
+import org.springframework.stereotype.Service;
 import ru.job4j.devops.enums.CalcEventType;
 import ru.job4j.devops.models.CalcEvent;
 import ru.job4j.devops.models.User;
 import ru.job4j.devops.repository.CalcEventRepository;
 
+@Service
 public class CalcService {
 
     private CalcEventRepository calcEventRepository;
+
+    public CalcService(CalcEventRepository calcEventRepository) {
+        this.calcEventRepository = calcEventRepository;
+    }
 
     public Long add(User user, int first, int second) {
         CalcEvent calcEvent = new CalcEvent();
